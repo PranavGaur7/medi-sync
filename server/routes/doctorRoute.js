f19710d04c59846f2ddd89ce9032e614c2e7e814
MohitRanjan2433 <126330385+MohitRanjan2433@users.noreply.github.com>
2024-03-20 21:44:29 +0530

    m

--- server/routes/doctorRoute.js
+++ server/routes/doctorRoute.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { login, signup } = require("../Controllers/Auth");
+const { login, signup } = require("../Controllers/auth");
 
 router.post("/login", login);
 router.post("/signup", signup);
