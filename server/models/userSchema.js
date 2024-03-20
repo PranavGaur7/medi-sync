4f1fb234acdc97462d4c3163628fdadedf2e7661
MohitRanjan2433 <126330385+MohitRanjan2433@users.noreply.github.com>
2024-03-21 00:20:34 +0530

    m

--- server/models/userSchema.js
+++ server/models/userSchema.js
@@ -12,6 +12,12 @@ const userSchema = new mongoose.Schema({
     password:{
         type:String,
         required:true,
+        validate:{
+            validator: function(value){
+                return value.length>=6;
+            },
+            message:"Password must be of length 6",
+        }
     },
     gender:{
         type:String,
